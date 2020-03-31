b63d4724e1151821dc0e3e8b0956283cc218f8b5
tirempbs <32857673+tirempbs@users.noreply.github.com>
2020-03-30 20:02:19 -0400

    Created New Game page.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NewGame msg="Welcome to Corona Charades"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewGame from './components/NewGame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewGame
   }
 }
 </script>
---
+++ src/components/NewGame.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="newGame">
+    <h1>Corona Charades</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
